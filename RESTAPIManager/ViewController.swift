c5b5d05f63f25f832e1a33254012e466c6a3fb26
Petros <psdeme@hotmail.com>
2016-12-21 16:14:08 +0200

    Initial Commit

---
+++ RESTAPIManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RESTAPIManager
+//
+//  Created by Petros Demetrakopoulos on 21/12/2016.
+//  Copyright © 2016 Petros Demetrakopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
