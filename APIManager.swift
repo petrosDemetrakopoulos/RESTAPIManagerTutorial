174cbe950c736392a8a2ac38d417a8e4439a2311
Petros <psdeme@hotmail.com>
2020-06-12 17:20:13 +0300

    - Changes to work correctly with Swift 4.0

--- APIManager.swift
+++ APIManager.swift
@@ -12,22 +12,21 @@ class APIManager: NSObject {
     
     let baseURL = "https://jsonplaceholder.typicode.com"
     static let sharedInstance = APIManager()
-   static let getPostsEndpoint = "/posts/"
+    static let getPostsEndpoint = "/posts/"
     
     func getPostWithId(postId: Int, onSuccess: @escaping(JSON) -> Void, onFailure: @escaping(Error) -> Void){
-    let url : String = baseURL + APIManager.getPostsEndpoint + String(postId)
-    let request: NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string: url)! as URL)
-    request.httpMethod = "GET"
-    let session = URLSession.shared
-    let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
-        if(error != nil){
-            onFailure(error!)
-        } else{
-            let result = JSON(data: data!)
-            onSuccess(result)
-        }
-    })
-    task.resume()
-}
-
+        let url : String = baseURL + APIManager.getPostsEndpoint + String(postId)
+        let request: NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
+            if(error != nil){
+                onFailure(error!)
+            } else{
+                let result = JSON(data: data!)
+                onSuccess(result)
+            }
+        })
+        task.resume()
+    }
 }
